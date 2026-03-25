2dec84f0ba5b17596cbafafba4a1e658c666a580
Alexander Evans <xander.evans@chess.com>
2026-03-25 13:20:22 -0600

    Fix convex schema validation for totalBreadCollected

--- convex/convex/schema.ts
+++ convex/convex/schema.ts
@@ -15,7 +15,7 @@ export default defineSchema({
     bestScore: v.number(),
     totalScore: v.number(),
     bread: v.number(),
-    totalBreadCollected: v.number(),
+    totalBreadCollected: v.optional(v.number()),
     recentScores: v.array(v.number()),
     beatenBots: v.array(v.string()),
 
